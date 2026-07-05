b2ea3c41529e33c92d9a71a9389d97f954151698
yogeshsingh0479 <40148123+yogeshsingh0479@users.noreply.github.com>
2026-07-05 23:53:00 +0530

    feature

--- src/main/java/load_service/controller/LoadController.java
+++ src/main/java/load_service/controller/LoadController.java
@@ -17,9 +17,9 @@ public class LoadController {
 	private LoadService service;
 	
 	@GetMapping("/{id}")
-    /*here i have added comment*/
 	public LoadData getLoadReport(@PathVariable Long id) {
 		LoadData data=service.getLoadReport(id);
+        
 		return data;
 	}
 
